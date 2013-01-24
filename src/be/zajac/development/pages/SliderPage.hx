b298c97bd6cc47a11c881f9f11f9c9c2de6d4694
Stojan Ilic <stolex@stojce.com>
2013-01-24 21:54:02 +0100

    Slider page now have horizontal and vertical slider for testing

--- src/be/zajac/development/pages/SliderPage.hx
+++ src/be/zajac/development/pages/SliderPage.hx
@@ -1,4 +1,5 @@
 package be.zajac.development.pages;
+import be.zajac.core.FWCore;
 import be.zajac.ui.Slider;
 
 /**
@@ -9,6 +10,7 @@ import be.zajac.ui.Slider;
 class SliderPage extends APage {
 
 	public var test: Slider;
+	public var testV:Slider;
 	
 	public function new() {
 		super();
@@ -18,14 +20,34 @@ class SliderPage extends APage {
 		test = new Slider();
 		addChild(test);
 		
+		testV = new Slider();
+		testV.direction = Slider.DIRECTION_VERTICAL;
+		testV.Width = FWCore.getHeightUnit();
+		testV.Height = FWCore.getHeightUnit() * 5;
+		addChild(testV);
+		
 		super.initialize();
 		title = 'Slider Example';
 	}
 	
 	override public function orderElements(): Void {
+		var c_x:Float;
+		var c_y:Float;
+		
 		super.orderElements();
-		test.x = (Width - test.Width) / 2;
-		test.y = (Height - test.Height) / 2;
+		
+		c_x = (Width - test.Width - testV.Width - 5) / 2;
+		c_y = (Height - test.Height - testV.Height - 5) / 2;
+		
+		test.x = c_x;
+		test.y = c_y;
+		
+		c_x += test.Width + 5;
+		c_y += test.Height + 5;
+		
+		testV.x = c_x;
+		testV.y = c_y;
+		
 	}
 	
 }
