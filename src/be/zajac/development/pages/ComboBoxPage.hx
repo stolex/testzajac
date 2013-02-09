2c4d8df3624247283c89f3de1501f0ab772601d5
Aleksandar Bogdanovic <comanjoni@gmail.com>
2013-02-09 20:16:41 +0100

    ComboBox test created

--- src/be/zajac/development/pages/ComboBoxPage.hx
+++ src/be/zajac/development/pages/ComboBoxPage.hx
@@ -1,4 +1,6 @@
 package be.zajac.development.pages;
+import be.zajac.core.FWCore;
+import be.zajac.ui.ComboBox;
 
 /**
  * ...
@@ -7,17 +9,44 @@ package be.zajac.development.pages;
 
 class ComboBoxPage extends APage {
 
+	public var testScroll: ComboBox;
+	public var testAutoSize: ComboBox;
+	
 	public function new() {
 		super();
 	}
 	
 	override public function initialize(): Void {
+		var c_items: Array<String>;
+		
+		testScroll = new ComboBox();
+		c_items = [];
+		for (i in 0...30) {
+			c_items.push('Item ' + i);
+		}
+		testScroll.items = c_items;
+		addChild(testScroll);
+		
+		
+		testAutoSize = new ComboBox();
+		c_items = [];
+		for (i in 0...3) {
+			c_items.push('Item ' + i);
+		}
+		testAutoSize.items = c_items;
+		addChild(testAutoSize);
+		
 		super.initialize();
 		title = 'ComboBox Example';
 	}
 	
 	override public function orderElements(): Void {
 		super.orderElements();
+		testScroll.x = (Width - testScroll.Width) / 2;
+		testScroll.y = (Height - testScroll.Height) / 2 - FWCore.getHeightUnit();
+		testAutoSize.x = (Width - testAutoSize.Width) / 2;
+		testAutoSize.y = testScroll.y + testScroll.Height + FWCore.getHeightUnit();
+		//testAutoSize.y = Height - testAutoSize.Height - 10;
 	}
 	
 }
